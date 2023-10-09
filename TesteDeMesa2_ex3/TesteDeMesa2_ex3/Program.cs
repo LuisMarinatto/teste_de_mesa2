6aecbcc4c2f0a98047fdcfd332b517f9c6a476de
Marinatto <fernandomarinatto@gmail.com>
2023-10-09 14:42:54 -0300

    Ex3

---
+++ TesteDeMesa2_ex3/TesteDeMesa2_ex3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Renda;
+            int periodoAnos = 6;
+
+            Console.WriteLine("informe o valor do investimento:");
+            double valorPresente = double.Parse(Console.ReadLine());
+            Console.WriteLine("informe a taxa de juros atual para essa ação:");
+            double taxaJuros = double.Parse(Console.ReadLine());
+
+            Renda = valorPresente * Math.Pow(1 + (taxaJuros / 100), (6));
+            Console.WriteLine(Renda.ToString("c"));
+        }
+    }
+}
