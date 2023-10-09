427922fbf8c4044e37de8d091b3bf0e30790150c
Marinatto <fernandomarinatto@gmail.com>
2023-10-09 11:47:44 -0300

    teste2_ex1

---
+++ TesteDeMesa2_ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+ 
+
+namespace Teste01
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double valorPresente = 1000, taxaJuros = 5.30 / 100;
+            int periodoMes = 6;
+
+			double valorFuturo = valorPresente * Math.Pow((1 + taxaJuros), periodoMes); 
+			Console.WriteLine(valorFuturo.ToString("c"));
+		}
+ 
+    }
+}
